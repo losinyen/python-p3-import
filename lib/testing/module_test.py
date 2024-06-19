ffaac1c0969be3822ae7c80ab0436531dd42483d
losinyen <nachuchphilip@gmail.com>
2024-06-19 10:54:53 +0300

    1 passed

--- lib/testing/module_test.py
+++ lib/testing/module_test.py
@@ -1,5 +1,21 @@
+""" from package1.module1 import function1
+
+function1() """
+
+""" from package1.subpackage1.module6 import function1
+function1() """
+
+# from the data tree assume we have a function1 we can access any module here.
+
+
+
+
+
+
+
 class TestModule:
     '''placeholder test'''
 
     def test_module(self):
+
         assert(1==1)
